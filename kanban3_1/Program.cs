875ea1e401ec126d6d4c485356aa7ace529a79b2
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2022-11-24 22:52:07 +0700

    first commit

---
+++ kanban3_1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+class Program
+{   
+    static void Main(String[] args)
+    {
+        Queue<char>queue1 = new Queue<char>();
+        char Flower = 'J';
+        int Count = 0;
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Please enter Flower");
+        Console.WriteLine("-------------------");
+        
+        while(Flower == 'J'||Flower == 'G'||Flower == 'O'||Flower == 'R')
+        {
+            Flower = char.Parse(Console.ReadLine());
+            
+            if (Flower == 'J')
+            {
+                AddJ(ref queue1);  
+            }
+            else if (Flower == 'G')
+            {   
+                AddG(ref queue1,ref Count);
+            }
+            else if (Flower == 'O')
+            {
+                AddO(ref queue1);
+            }
+            else if (Flower == 'R')
+            {
+                AddR(ref queue1);
+            }
+        }
+
+        static void AddJ(ref Queue<char> queue1)
+        {
+            {
+                queue1.Push('J');
+            }
+        }
+
+        static void AddG(ref Queue<char> queue1,ref int Count)
+        {
+            
+            if(Count < 3)
+            {
+             queue1.Push('G');
+             Count++;   
+            }
+            else 
+            {
+               Console.WriteLine("Invalid pattern."); 
+            }
+        }
+      
+        static void AddO(ref Queue<char> queue1)
+        {
+            {
+                queue1.Push('O');
+            }
+        } 
+
+        static void AddR(ref Queue<char> queue1)
+        {
+            if(queue1.GetLength() != 0)
+            {
+                queue1.Push('R');
+            }
+            else
+            {
+               Console.WriteLine("Invalid pattern."); 
+            }
+        }
+
+        Console.Write("Output: ");
+        for(int n = 0; n < queue1.GetLength();n++)
+        {
+            Console.Write("{0}",queue1.Get(n));   
+        }
+    }   
+}
---
+++ kanban3_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+class Program
+{   
+    static void Main(String[] args)
+    {
+        Queue<int>queue1 = new Queue<int>();
+        Queue<int>queue2 = new Queue<int>();
+        Queue<int>queue3 = new Queue<int>();
+        char Size = 'L';
+        
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("Please enter fruit size");
+        Console.WriteLine("-----------------------");
+        
+        while(Size == 'L'||Size == 'M'||Size == 'S')
+        {
+            Size = char.Parse(Console.ReadLine());
+            
+            if (Size == 'L')
+            {
+                queue1.Push(1);
+                CutL(ref queue2,ref queue3);  
+            }
+            else if (Size == 'M')
+            {
+                queue1.Push(2);
+                CutM(ref queue2);
+            }
+            else if (Size == 'S')
+            {
+                queue1.Push(3);
+            }
+        }
+
+        static void CutL(ref Queue<int> queue2, ref Queue<int> queue3)
+        {
+            queue2.Push(2);
+            queue2.Push(2);
+
+            queue3.Push(3);
+            queue3.Push(3);
+            queue3.Push(3);
+            queue3.Push(3);
+            queue3.Push(3);
+            queue3.Push(3);
+        }
+
+        static void CutM(ref Queue<int> queue2)
+        {
+            queue2.Push(3);
+            queue2.Push(3);
+            queue2.Push(3);
+        }
+
+        Console.Write("Output: ");
+        for(int n = 0; n < queue1.GetLength();n++)
+        {
+            Console.Write("{0}",queue1.Get(n));   
+        }
+        for(int n = 0; n < queue2.GetLength();n++)
+        {
+            Console.Write("{0}",queue2.Get(n));
+        }
+        for(int n = 0; n < queue3.GetLength();n++)
+        {
+            Console.Write("{0}", queue3.Get(n));
+        }
+    }   
+}
